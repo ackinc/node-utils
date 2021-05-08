c2129f72f47ce801fbc52f882ee3521d03ae8069
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2021-05-08 22:23:03 +0530

    Added group-by-multi-keys

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const generateRandomString = require("./lib/generate-random-string");
+const groupByMultiKeys = require("./lib/group-by-multi-keys");
 
-module.exports = { generateRandomString };
+module.exports = { generateRandomString, groupByMultiKeys };
